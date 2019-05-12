95cc58079f9ef009d3196a4506fc3d99fb1809d1
Sam Jones <samueldjones24@gmail.com>
2019-05-12 14:56:55 +0100

    Added CSS to Property Card

--- src/components/properties.jsx
+++ src/components/properties.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropertyCard from './property-card';
 import axios from 'axios';
 import '../css/properties.css';
+import { Link } from 'react-router-dom';
 
 const URL = 'http://localhost:3000/api/v1/PropertyListing';
 
@@ -17,19 +18,37 @@ class Properties extends React.Component {
 
   componentDidMount() {
     axios.get(URL)
-      .then((response) => this.setState({ properties: response.data }))
+      .then(({ data: properties }) => this.setState({ properties }))
       .catch(() => {
         this.setState({
           isError: true,
-          alertMessage: 'Sorry, we could not find any properties.',
+          alertMessage: 'Sorry, we could not find any properties. Please try again.',
         });
       });
   }
 
+  componentDidUpdate(prevProps) {
+    const { search } = this.props.location;
+
+    if (prevProps.location.search !== search) {
+      axios.get(`URL${search}`)
+        .then(({ data: properties }) => this.setState({ properties }))
+        .catch(err => console.error(err));
+    }
+  }
+
   render() {
     return (
-      <div className="properties">
-        {this.state.properties.map((property) => <PropertyCard key={property._id} {...property} />)}
+      <div>
+        <div className="sidebar">
+          <Link to={'/?query={ "city": "Manchester" })'}>Manchester</Link>
+          <Link to={'/?query={ "city": "Leeds" })'}>Leeds</Link>
+          <Link to={'/?query={ "city": "Sheffield" })'}>Sheffield</Link>
+          <Link to={'/?query={ "city": "Liverpool" })'}>Liverpool</Link>
+        </div>
+        <div className="properties">
+          {this.state.properties.map(property => <div key={property._id} className="col"> <PropertyCard {...property} /> </div>)}
+        </div>
       </div>
     );
   }
--- src/components/property-card.jsx
+++ src/components/property-card.jsx
@@ -1,28 +1,35 @@
 import React from 'react';
 import '../css/property-card.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBed, faBath, faPoundSign, faEnvelope 
+} from '@fortawesome/free-solid-svg-icons';
 
 const PropertyCard = props => (
-  <div className="property-card">
-    <div className="title">
-      <span>{props.title}</span>
-    </div>
-    <div className="type">
-      <span>{props.type}</span>
-    </div>
-    <div className="bathrooms">
-      <span>{props.bathrooms}</span>
-    </div>
-    <div className="bedrooms">
-      <span>{props.bedrooms}</span>
-    </div>
-    <div className="price">
-      <span>{props.price}</span>
-    </div>
-    <div className="city">
-      <span>{props.city}</span>
-    </div>
-    <div className="email">
-      <span>{props.email}</span>
+  <div>
+    <div className="property-card">
+      <div className="title">
+        <span>{props.title}</span>
+      </div>
+      <div className="type">
+        <span>{props.type} - {props.city}</span>
+      </div>
+      <div className="bathrooms">
+        <FontAwesomeIcon icon={faBath} />
+        <span> {props.bathrooms}</span>
+      </div>
+      <div className="bedrooms">
+        <FontAwesomeIcon icon={faBed} />
+        <span> {props.bedrooms}</span>
+      </div>
+      <div className="price">
+        <FontAwesomeIcon icon={faPoundSign} />
+        <span> {props.price}</span>
+      </div>
+      <div className="email">
+        <FontAwesomeIcon icon={faEnvelope} />
+        <a href="mailto:someone@example.com?Subject=Hello%20again" target="_top"> E-mail us</a>
+      </div>
     </div>
   </div>
 );
